3ac7bea2b453e12ce2c328f9113c79db6be1c258
limesnej <38116855+limesnej@users.noreply.github.com>
2021-08-24 09:19:32 +0200

    added function

--- numbers.py
+++ numbers.py
@@ -1,3 +1,16 @@
 import math
 
-print(math.pi)
+
+def calcPi(roundedVal):
+
+    pi = math.pi
+
+    rounded = round(pi, roundedVal)
+
+    return rounded
+
+daInput = int(input("How many digits do you want in your pi? "))
+
+piPieces = calcPi(daInput)
+
+print(f"Here you go: {piPieces}")
